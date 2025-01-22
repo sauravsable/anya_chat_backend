7b5b7496f5807fd3e393f1790c72ac679c7c9e4a
sauravsable <sauravsable4102@gmail.com>
2025-01-22 22:04:20 +0530

    jwt added

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,7 @@
 module.exports = ({ env }) => ({
-    // ...
-    "users-permissions": {
-      config: {
-        register: {
-          allowedFields: ["nickname"],
-        },
-      },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'qf4TV4qZT0kPvLfghyq5Yg=='),
     },
-    // ...
-  });
+  },
+});
