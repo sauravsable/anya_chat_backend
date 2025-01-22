7584f005daf11aaa517f2f113b9c027eedf8db99
sauravsable <sauravsable4102@gmail.com>
2025-01-22 21:59:12 +0530

    api keys added

--- config/server.js
+++ config/server.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   websockets: true,
+  app: {
+    keys: ['1f5c4e6a8d5a8bcd6f7e4a3b2c1d8e6f', 'a9b8c7d6e5f4a3b2c1d8f7e6a5b4c3d2'],
+  },
 });
